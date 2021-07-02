d1c2be60e00b8f931ff64e0869a121c41e336d4f
CaioReuter <caioreuter@gmail.com>
2021-07-02 16:15:48 -0300

    Alterações arquivo Cadastro.js

--- Sagitta/ClientApp/src/components/Cadastro.js
+++ Sagitta/ClientApp/src/components/Cadastro.js
@@ -1,76 +1,77 @@
 ﻿import React, { Component } from 'react';
 
+
 export class Cadastro extends Component {
     static displayName = Cadastro.name;
 
     constructor(props) {
         super(props);
-        this.state = { value: "", nome:"", pessoa: "" };
-
-        this.handleChange = this.HandleChange.bind(this);
-        this.handleChange1 = this.handleChange1.bind(this);
+        this.state = {
+            nome: null,
+            cpf: null,
+            idade: null,
+            sexo: null,
+            telefone: null,
+            email: null,
+            dataNasc: null,
+            cep: null,
+            cidade: null,
+            estado: null,
+            temComorbidade: null        
+        }
     }
 
-    HandleChange(event) {
-        this.setState({ value: event.target.value.replace(/[^\d]/g, "") });
+    onChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
     }
 
-    handleChange1(event) {
-        this.setState({ nome: event.target.value });
+    onSubmit(e) {
+        e.preventDefault();
+        let nome = this.state.nome;
+        let cpf = this.state.cpf;
+        let idade = this.state.idade;
+        let sexo = this.state.sexo;
+        let telefone = this.state.telefone;
+        let email = this.state.email;
+        let dataNasc = this.state.dataNasc;
+        let cep = this.state.cep;
+        let cidade = this.state.cidade;
+        let estado = this.state.estado;
+        let temComorbidade = this.state.temComorbidade;
+
+        alert('DADOS: ' + nome + cpf + idade + sexo + telefone + email + dataNasc + cep + cidade + estado + temComorbidade);
+   
     }
-
-    //componentDidMount() {
-    //    // Simple POST request with a JSON body using fetch
-    //    const requestOptions = {
-    //        method: 'POST',
-    //        headers: { 'Content-Type': 'application/json' },
-    //        body: JSON.stringify({ title: 'React POST Request Example' })
-    //    };
-    //    fetch('pessoa', requestOptions)
-    //        .then(response => response.json())
-    //        .then(data => this.setState({ postId: data.id }));
-    //}
-
-    handleOnSubmit(event) {
-        alert('Um nome foi enviado: ' + this.state.nome);
-        event.preventDefault();
-  
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ title: 'React POST Request Example' })
-        };
-
-        fetch('pessoa', requestOptions);
-    };
 
     render() {
         return (
             <div>
                 <h3 className="espaco texto">PREENCHA COM SEUS DADOS</h3>
-                <form id="form1" className="areaForm areaform2" onSubmit={this.handleOnSubmit}>
+                <form className="areaForm areaform2" onSubmit={this.onSubmit.bind(this)}>
 
-
+                    
                     <div className="areaCadastro">
-
+                          
                         <div className="form-group">
                             <div className="form-group col-md-6">
                                 <label for="nome">NOME</label>
-                                <input type="text" value={this.state.nome} onChange={this.handleChange1} className="form-control" id="nome" placeholder="Nome" autoComplete="off" />
+                                <input type="text" name="nome" onChange={this.onChange.bind(this)} className="form-control" id="nome" placeholder="Nome" autoComplete="off" />
                             </div>
                             <div className="form-group col-md-6">
                                 <label for="cpf">CPF</label>
-                                <input type="text" maxLength="11" className="form-control" id="cpf" placeholder="Cpf" autoComplete="off" />
+                                <input type="text" maxLength="11" name="cpf" onChange={this.onChange.bind(this)} className="form-control" id="cpf" placeholder="Cpf" autoComplete="off" />
                             </div>
                         </div>
                         <div className="form-group">
                             <div className="form-group col-md-6">
                                 <label for="idade">IDADE</label>
-                                <input type="text" className="form-control" value={this.state.value} maxLength="3" onChange={this.HandleChange} id="idade" placeholder="Idade" autoComplete="off" />
+                                <input type="text" className="form-control" name="idade" onChange={this.onChange.bind(this)} maxLength="3" id="idade" placeholder="Idade" autoComplete="off" />
                             </div>
                             <div className="form-group col-md-6">
                                 <label for="sexo">SEXO</label>
-                                <select className="form-control" id="sexo">
+                                <select className="form-control" id="sexo" name="sexo" onChange={this.onChange.bind(this)}>
                                     <option selected>Escolha</option>
                                     <option value="1">Masculino</option>
                                     <option value="2">Feminino</option>
@@ -80,37 +81,37 @@ export class Cadastro extends Component {
                         <div className="form-group">
                             <div className="form-group col-md-6">
                                 <label for="telefone">Telefone</label>
-                                <input type="text" className="form-control" id="telefone" maxLength="11" placeholder="Telefone" autoComplete="off" />
+                                <input type="text" className="form-control" name="telefone" onChange={this.onChange.bind(this)} id="telefone" maxLength="11" placeholder="Telefone" autoComplete="off" />
                             </div>
                             <div className="form-group col-md-6">
                                 <label for="email">EMAIL</label>
-                                <input type="email" className="form-control" id="email" placeholder="nome@exemplo.com" autoComplete="off" />
+                                <input type="email" className="form-control" name="email" onChange={this.onChange.bind(this)} id="email" placeholder="nome@exemplo.com" autoComplete="off" />
                             </div>
                         </div>
                         <div className="form-group">
                             <div className="form-group col-md-6">
                                 <label for="dataNasc">DATA DE NASCIMENTO</label>
-                                <input type="date" className="form-control" id="dataNasc" />
+                                <input type="date" className="form-control" name="dataNasc" onChange={this.onChange.bind(this)} id="dataNasc" />
                             </div>
                             <div className="form-group col-md-6">
                                 <label for="cep">CEP</label>
-                                <input type="text" className="form-control" id="cep" maxLength="8" placeholder="Cep" autoComplete="off" />
+                                <input type="text" className="form-control" name="cep" onChange={this.onChange.bind(this)} id="cep" maxLength="8" placeholder="Cep" autoComplete="off" />
                             </div>
                         </div>
                         <div className="form-group">
                             <div className="form-group col-md-6">
                                 <label for="cidade">CIDADE</label>
-                                <input type="text" className="form-control" id="cidade" placeholder="Cidade" autoComplete="off" />
+                                <input type="text" className="form-control" name="cidade" onChange={this.onChange.bind(this)} id="cidade" placeholder="Cidade" autoComplete="off" />
                             </div>
                             <div className="form-group col-md-6">
                                 <label for="estado">Estado</label>
-                                <input type="text" className="form-control" id="estado" placeholder="Estado" autoComplete="off" />
+                                <input type="text" className="form-control" name="estado" onChange={this.onChange.bind(this)} id="estado" placeholder="Estado" autoComplete="off" />
                             </div>
                         </div>
                         <div className="form-group">
                             <div className="form-group col-md-6">
                                 <label for="temComorbidade">Tem comorbidade</label>
-                                <select className="form-control" id="temComorbidade">
+                                <select className="form-control" id="temComorbidade" name="temComorbidade" onChange={this.onChange.bind(this)}>
                                     <option selected>Escolha</option>
                                     <option value="1">Sim</option>
                                     <option value="2">Não</option>
@@ -128,4 +129,6 @@ export class Cadastro extends Component {
             </div>
         );
     }
+
+
 }
